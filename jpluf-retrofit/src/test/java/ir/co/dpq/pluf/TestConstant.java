a6790f2fd46844a35c586d6502e3a4c8ddc6fc98
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2015-12-09 15:50:04 +0330

    کل پروژه از Pluf انتقال پیدا کرده است.

---
+++ jpluf-retrofit/src/test/java/ir/co/dpq/pluf/TestConstant.java
@@ -0,0 +1,8 @@
+package ir.co.dpq.pluf;
+
+import ir.co.dpq.pluf.test.TestCoreConstant;
+
+public class TestConstant extends TestCoreConstant {
+
+	public static final String API_URL = "http://localhost:1380";
+}
