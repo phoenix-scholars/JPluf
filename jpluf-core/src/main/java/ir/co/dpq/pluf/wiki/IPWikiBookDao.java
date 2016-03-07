d8212de44454daba2096b66d63cfd55228b2f5ef
hadi <mohammad.hadi.mansouri@chmail.ir>
2016-03-07 12:53:54 +0330

    اضافه کردن متدهای با پارامتر callback به PBook

--- jpluf-core/src/main/java/ir/co/dpq/pluf/wiki/IPWikiBookDao.java
+++ jpluf-core/src/main/java/ir/co/dpq/pluf/wiki/IPWikiBookDao.java
@@ -2,6 +2,7 @@ package ir.co.dpq.pluf.wiki;
 
 import java.util.List;
 
+import ir.co.dpq.pluf.IPCallback;
 import ir.co.dpq.pluf.IPPaginatorPage;
 import ir.co.dpq.pluf.PPaginatorParameter;
 import ir.co.dpq.pluf.km.PCategory;
@@ -18,36 +19,53 @@ import ir.co.dpq.pluf.user.PUser;
 public interface IPWikiBookDao {
 
 	PWikiBook createWikiBook(PWikiBook book);
+	void createWikiBook(PWikiBook book, IPCallback<PWikiBook> callback);
 
 	PWikiBook getWikiBook(Long bookId);
+	void getWikiBook(Long bookId, IPCallback<PWikiBook> callback);
 
 	PWikiBook updateWikiBook(PWikiBook book);
+	void updateWikiBook(PWikiBook book, IPCallback<PWikiBook> callback);
 
 	PWikiBook deleteWikiBook(PWikiBook book);
+	void deleteWikiBook(PWikiBook book, IPCallback<PWikiBook> callback);
 
 	IPPaginatorPage<PWikiBook> findWikiBook(PPaginatorParameter param);
+	void findWikiBook(PPaginatorParameter param, IPCallback<IPPaginatorPage<PWikiBook>> callback);
 
 	PWikiBook addLabelToBook(PWikiBook book, PLabel label);
+	void addLabelToBook(PWikiBook book, PLabel label, IPCallback<PWikiBook> callback);
 
 	List<PLabel> getBookLabels(PWikiBook book);
+	void getBookLabels(PWikiBook book, IPCallback<List<PLabel>> callback);
 
 	PWikiBook deleteLabelFromBook(PWikiBook book, PLabel label);
+	void deleteLabelFromBook(PWikiBook book, PLabel label, IPCallback<PWikiBook> callback);
 
-	PWikiPage addBookToCategory(PWikiBook book, PCategory category);
+	PWikiBook addBookToCategory(PWikiBook book, PCategory category);
+	void addBookToCategory(PWikiBook book, PCategory category, IPCallback<PWikiBook> callback);
 
-	PWikiPage deleteCategoryFromBook(PWikiBook book, PCategory category);
+	PWikiBook deleteCategoryFromBook(PWikiBook book, PCategory category);
+	void deleteCategoryFromBook(PWikiBook book, PCategory category, IPCallback<PWikiBook> callback);
 
 	List<PCategory> getBookCategories(PWikiBook book);
+	void getBookCategories(PWikiBook book, IPCallback<List<PCategory>> callback);
 
 	IPPaginatorPage<PWikiPageItem> getBookPages(PWikiBook book, PPaginatorParameter param);
+	void getBookPages(PWikiBook book, PPaginatorParameter param, IPPaginatorPage<PWikiPageItem> callback);
 
-	PWikiPage addPageToBook(PWikiBook book, PWikiPage page);
+	PWikiBook addPageToBook(PWikiBook book, PWikiPage page);
+	void addPageToBook(PWikiBook book, PWikiPage page, IPCallback<PWikiBook> callback);
 
-	PWikiPage deletePageFromBook(PWikiBook book, PWikiPage page);
+	PWikiBook deletePageFromBook(PWikiBook book, PWikiPage page);
+	PWikiBook deletePageFromBook(PWikiBook book, PWikiPage page, IPCallback<PWikiBook> callback);
 
-	PWikiPage addInterestedUser(PWikiBook book);
+	PWikiBook addInterestedUser(PWikiBook book);
+	void addInterestedUser(PWikiBook book, IPCallback<PWikiBook> callback);
 
-	PWikiPage deleteInterestedUser(PWikiBook book);
+	PWikiBook deleteInterestedUser(PWikiBook book);
+	void deleteInterestedUser(PWikiBook book, IPCallback<PWikiBook> callback);
 
 	IPPaginatorPage<PUser> getBookInteresteds(PWikiBook book, PPaginatorParameter param);
+	void getBookInteresteds(PWikiBook book, PPaginatorParameter param, IPCallback<IPPaginatorPage<PUser>> callback);
 }
